36ae408a97b14c37f20616a84abec1740984f6ae
John Ndungu <ndungulink@gmail.com>
2025-04-02 08:33:19 +0300

    conditional statements

--- Control Flow/app.js
+++ Control Flow/app.js
@@ -1,3 +1,18 @@
 // if (condition) {...}
 // else if (condition) {...}
 // else {...}
+
+//conditional statements
+let time = 10;
+let greetings;
+
+if (time < 10) {
+  greetings = "Good Morning";
+  console.log(greetings);
+} else if (time < 20) {
+  greetings = "Good Day";
+  console.log(greetings);
+} else {
+  greetings = "Good Evening";
+  console.log(greetings);
+}
