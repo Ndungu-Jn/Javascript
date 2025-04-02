6b5f976d2796be2d747daa49e442ab9571e7b45f
John Ndungu <ndungulink@gmail.com>
2025-04-02 08:50:57 +0300

    switch statements

--- Control Flow/app.js
+++ Control Flow/app.js
@@ -3,16 +3,55 @@
 // else {...}
 
 //conditional statements
-let time = 10;
-let greetings;
+// let time = 10;
+// let greetings;
 
-if (time < 10) {
-  greetings = "Good Morning";
-  console.log(greetings);
-} else if (time < 20) {
-  greetings = "Good Day";
-  console.log(greetings);
-} else {
-  greetings = "Good Evening";
-  console.log(greetings);
+// if (time < 10) {
+//   greetings = "Good Morning";
+//   console.log(greetings);
+// } else if (time < 20) {
+//   greetings = "Good Day";
+//   console.log(greetings);
+// } else {
+//   greetings = "Good Evening";
+//   console.log(greetings);
+// }
+
+// let password = 7;
+
+// if (password === 8) {
+//   console.log("Welcome");
+// } else if (password <= 8) {
+//   console.log("password is too short");
+// } else if (password >= 8) {
+//   console.log("Too long password");
+//   ("Password should be 8 character");
+// } else {
+//   console.log("Please provide  a password");
+// }
+
+// switch (expression){
+//     case x:
+//         //print
+//         //break;
+//     case y:
+//         //print
+//         //break;
+// }
+
+let x = 1;
+let bulb;
+
+switch (x) {
+  case 0:
+    bulb = "off";
+    console.log(bulb);
+    break;
+  case 1:
+    bulb = "on";
+    console.log(bulb);
+    break;
+  default:
+    bulb = "no value found";
+    console.log(bulb);
 }
