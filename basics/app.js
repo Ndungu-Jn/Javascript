2f716a9e98d759a155da01f860f44292d582417b
John Ndungu <ndungulink@gmail.com>
2025-04-02 07:43:25 +0300

    type conversion

--- basics/app.js
+++ basics/app.js
@@ -2,3 +2,13 @@
 //clg then tab or
 //log then tab.
 console.log("Shortcut for writing the console.log");
+
+let money = "50";
+console.log(money);
+console.log(typeof money);
+
+// - Convert string to number
+
+money = parseInt(money);
+console.log(money);
+console.log(typeof money);
