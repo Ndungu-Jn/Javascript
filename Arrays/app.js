d4e5a648556c7d5057a441c10d6a0a94674d7e86
John Ndungu <ndungulink@gmail.com>
2025-04-02 10:50:07 +0300

    Objects

--- Arrays/app.js
+++ Arrays/app.js
@@ -1,21 +1,52 @@
-//Arrays -- 0 index based
-// empty array
-// let myList = [1, 2, 3, 4, 5];
-// console.log(myList);
+// //Arrays -- 0 index based
+// // empty array
+// // let myList = [1, 2, 3, 4, 5];
+// // console.log(myList);
 
-const fruits = ["apples", "pomegranate", "mango"];
-const moreFruits = ["strawberries", "pineapple", "grapefruit"];
-const totalFruits = fruits.concat(moreFruits);
+// const fruits = ["apples", "pomegranate", "mango"];
+// const moreFruits = ["strawberries", "pineapple", "grapefruit"];
+// const totalFruits = fruits.concat(moreFruits);
 
-console.log(totalFruits);
-// fruits.push("banana");
-// console.log(fruits);
+// console.log(totalFruits);
+// // fruits.push("banana");
+// // console.log(fruits);
 
-// fruits.pop();
-// console.log(fruits);
+// // fruits.pop();
+// // console.log(fruits);
 
-// fruits.shift();
-// console.log(fruits);
+// // fruits.shift();
+// // console.log(fruits);
 
-// fruits.unshift("orange");
-// console.log(fruits);
+// // fruits.unshift("orange");
+// // console.log(fruits);
+
+//Objets -- key value arrangement -- basivally  like dictionariesin python
+
+const person = {
+  //  key     //Value
+  firstName: "Ndungu",
+  lastName: "what",
+  age: 25,
+  location: ["planet", "Earth"],
+  isProgrammer: true,
+};
+
+console.log(person);
+// accessing Items
+
+console.log(person.firstName);
+console.log(person.age);
+console.log(person.location);
+console.log(person.lastName);
+
+const car = {
+  type: "Nissan",
+  model: "prado",
+  color: "indigo",
+};
+
+console.log((car.type = "Toyota"));
+console.log(car);
+
+car.wheels = 4;
+console.log(car);
