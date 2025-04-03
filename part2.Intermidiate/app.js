d482e60581f339d9e3e9ff0678e3c54383050eb1
John Ndungu <ndungulink@gmail.com>
2025-04-03 18:01:14 +0300

    spread operator

--- part2.Intermidiate/app.js
+++ part2.Intermidiate/app.js
@@ -49,12 +49,40 @@
 
 // ratings();
 
-function multiply(a, b = 1) {
-  return a * b;
+// function multiply(a, b = 1) {
+//   return a * b;
+// }
+
+// console.log(multiply(3));
+
+// const multiply = (a, b = 1) => a * b;
+
+// console.log(multiply(4));
+
+// spread operator -- takes in a iterable (eg an array) and expands it into individual Elements
+//used to make shallow copies of JS objects
+// enhances readabilty.
+// we use ... as the spreader.
+
+function giveMe4(a, b, c, d) {
+  console.log("a", a);
+  console.log("b", b);
+  console.log("c", c);
+  console.log("d", d);
 }
 
-console.log(multiply(3));
+const colors = ["red", "green", "blue", "teal"];
 
-const multiply = (a, b = 1) => a * b;
+giveMe4(...colors);
 
-console.log(multiply(4));
+const strNums = ["one", "two", "three"];
+const moreStrNums = ["four", "five", "six"];
+const concat = [...strNums, ...moreStrNums];
+
+console.log(concat);
+
+const obj1 = { x: 1, y: 2 };
+const obj2 = { z: 3 };
+const conc = { ...obj1, ...obj2 };
+
+console.log(conc);
