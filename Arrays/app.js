2b33c6a23d0cd731a06297ccd722e7b896e33d61
John Ndungu <ndungulink@gmail.com>
2025-04-03 09:17:25 +0300

    dates and time

--- Arrays/app.js
+++ Arrays/app.js
@@ -154,3 +154,15 @@
 // console.log(person.greet());
 
 //JSON -- Javascript object notation
+
+// DATES
+
+const date = new Date();
+
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDay();
+
+console.log(`Year: ${year}`);
+console.log(`Month: ${month}`);
+console.log(`Day: ${day}`);
