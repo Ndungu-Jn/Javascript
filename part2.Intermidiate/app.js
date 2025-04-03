7724ffab1a0b2983642caee4453fdce8263cfe14
John Ndungu <ndungulink@gmail.com>
2025-04-03 10:10:42 +0300

    Arrow Functions

--- part2.Intermidiate/app.js
+++ part2.Intermidiate/app.js
@@ -1,12 +1,29 @@
 // string literals
-const templateString = `The quick
-brown fox
-jumps over
-the lazy dog`;
+// const templateString = `The quick
+// brown fox
+// jumps over
+// the lazy dog`;
 
-console.log(templateString);
+// console.log(templateString);
 
-let firstName = "John";
-let lastName = "Doe";
+// let firstName = "John";
+// let lastName = "Doe";
 
-console.log(`my name is ${firstName} ${lastName}`);
+// console.log(`my name is ${firstName} ${lastName}`);
+
+// Arrow functions -- shorter way to write functions
+
+// basic way
+
+function greet(username) {
+  return `Hello ${username}`;
+}
+console.log(greet("John"));
+
+// now the arrow function way
+greet = (username) => `Hello ${username}`;
+
+console.log(greet("John"));
+
+const double = (number) => number * 2;
+console.log(double(4));
